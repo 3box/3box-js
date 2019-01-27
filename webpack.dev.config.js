a9581b0822db86cdf08668fa4285d00f680b9992
Zach Ferland <zachferland@gmail.com>
2019-01-26 20:41:00 -0500

    build: only allow env var for dev build and add watch mode for dev

---
+++ webpack.dev.config.js
@@ -0,0 +1,23 @@
+// const path = require('path');
+const webpack = require('webpack')
+
+// const webpackBaseConfig = require('webpack.base.config.js')
+//
+// webpackBaseConfig['plugins'] = [
+//   new webpack.EnvironmentPlugin(['PINNING_NODE'])
+// ]
+
+module.exports = Object.assign(require('./webpack.config.js'), {
+  watch: true,
+  plugins: [
+    new webpack.EnvironmentPlugin([
+      'ADDRESS_SERVER_URL',
+      'PINNING_NODE',
+      'PINNING_ROOM',
+      'IFRAME_STORE_VERSION',
+      'IFRAME_STORE_URL',
+      'GRAPHQL_SERVER_URL',
+      'PROFILE_SERVER_URL',
+    ])
+  ]
+})
