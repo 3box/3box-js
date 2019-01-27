7ccc2afd06b3236247550a6cd80a442e3bf56a10
Zach Ferland <zachferland@gmail.com>
2019-01-26 20:16:12 -0500

    ref: add option for env variables to be used

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const IFRAME_STORE_VERSION = '0.0.3'
 
 module.exports =  {
-  address_server_url: 'https://beta.3box.io/address-server',
-  pinning_node: '/dnsaddr/ipfs.3box.io/tcp/443/wss/ipfs/QmZvxEpiVNjmNbEKyQGvFzAY1BwmGuuvdUTmcTstQPhyVC',
-  pinning_room: '3box-pinning',
-  iframe_store_version: IFRAME_STORE_VERSION,
-  iframe_store_url: `https://iframe.3box.io/${IFRAME_STORE_VERSION}/iframe.html`,
+  address_server_url: process.env.ADDRESS_SERVER_URL || 'https://beta.3box.io/address-server',
+  pinning_node: process.env.PINNING_NODE || '/dnsaddr/ipfs.3box.io/tcp/443/wss/ipfs/QmZvxEpiVNjmNbEKyQGvFzAY1BwmGuuvdUTmcTstQPhyVC',
+  pinning_room: process.env.PINNING_ROOM || '3box-pinning',
+  iframe_store_version: process.env.IFRAME_STORE_VERSION || IFRAME_STORE_VERSION,
+  iframe_store_url: process.env.IFRAME_STORE_URL || `https://iframe.3box.io/${IFRAME_STORE_VERSION}/iframe.html`,
   ipfs_options: {
     EXPERIMENTAL: {
       pubsub: true
@@ -15,6 +15,6 @@ module.exports =  {
       Bootstrap: [ ]
     }
   },
-  graphql_server_url: 'https://aic67onptg.execute-api.us-west-2.amazonaws.com/develop/graphql',
-  profile_server_url: 'https://ipfs.3box.io'
+  graphql_server_url: process.env.GRAPHQL_SERVER_URL || 'https://aic67onptg.execute-api.us-west-2.amazonaws.com/develop/graphql',
+  profile_server_url: process.env.PROFILE_SERVER_URL || 'https://ipfs.3box.io'
 }
