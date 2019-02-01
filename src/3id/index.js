744dcaa2f696a47d35209a143a0debf7f37b6a4c
Joel Torstensson <oed3933@gmail.com>
2019-02-01 20:24:57 +0100

    ref(3id): removes muport-core dependancy

--- src/3id/index.js
+++ src/3id/index.js
@@ -1,6 +1,6 @@
 const { HDNode } = require('ethers').utils
-const MuPort = require('muport-core')
 const didJWT = require('did-jwt')
+const IpfsMini = require('ipfs-mini')
 const localstorage = require('store')
 const utils = require('../utils/index')
 const Keyring = require('./keyring')
@@ -10,10 +10,10 @@ const STORAGE_KEY = 'serialized3id_'
 const MUPORT_IPFS = { host: config.muport_ipfs_host, port: config.muport_ipfs_port, protocol: config.muport_ipfs_protocol}
 
 class ThreeId {
-  constructor (serializeState, ethereum, opts) {
+  constructor (serializeState, ethereum, ipfs, opts) {
     this._ethereum = ethereum
+    this._ipfs = ipfs
     this._keyrings = {}
-    opts = Object.assign({ muportIpfs: MUPORT_IPFS }, opts )
     this._init3id(serializeState, opts)
     localstorage.set(STORAGE_KEY + this.managementAddress, this.serializeState())
   }
@@ -21,14 +21,13 @@ class ThreeId {
   async signJWT (payload) {
     const settings = {
       signer: this._mainKeyring.getJWTSigner(),
-      //issuer: this.getDid()
-      issuer: this._muport.getDid()
+      issuer: this.getDid()
     }
     return didJWT.createJWT(payload, settings)
   }
 
   getDid () {
-    return this._muport.getDid()
+    return this._muportDID
   }
 
   serializeState () {
@@ -36,7 +35,6 @@ class ThreeId {
       managementAddress: this.managementAddress,
       seed: this._mainKeyring.serialize(),
       spaceSeeds: {},
-      muport: this._muport.serializeState()
     }
     Object.keys(this._keyrings).map(name => {
       stateObj.spaceSeeds[name] = this._keyrings[name].serialize()
@@ -44,15 +42,28 @@ class ThreeId {
     return JSON.stringify(stateObj)
   }
 
-  _init3id (serializeState, opts) {
+  _init3id (serializeState) {
     const state = JSON.parse(serializeState)
     this.managementAddress = state.managementAddress
     this._mainKeyring = new Keyring(state.seed)
-    this._muport = new MuPort(state.muport, opts.muportIPFS || {} )
     Object.keys(state.spaceSeeds).map(name => {
       this._keyrings[name] = new Keyring(state.spaceSeeds[name])
     })
-    this.muportFingerprint = utils.sha256Multihash(this._muport.getDid())
+  }
+
+  async _initMuport (muportIpfs) {
+    let keys = this._mainKeyring.getPublicKeys()
+    const doc = createMuportDocument(keys.signingKey, this.managementAddress, keys.asymEncryptionKey)
+    let docHash = (await this._ipfs.files.add(Buffer.from(JSON.stringify(doc))))[0].hash
+    this._muportDID = 'did:muport:' + docHash
+    this.muportFingerprint = utils.sha256Multihash(this._muportDID)
+    const publishToInfura = async () => {
+      const ipfsMini = new IpfsMini(muportIpfs)
+      ipfsMini.addJSON(doc, (err, res) => {
+        if (err) console.error(err)
+      })
+    }
+    publishToInfura()
   }
 
   getKeyringBySpaceName (name) {
@@ -85,8 +96,7 @@ class ThreeId {
     return Boolean(localstorage.get(STORAGE_KEY + address.toLowerCase()))
   }
 
-  static async getIdFromEthAddress (address, ethereum, opts = {}) {
-    opts = Object.assign({ muportIpfs: MUPORT_IPFS }, opts )
+  static async getIdFromEthAddress (address, ethereum, ipfs, opts = {}) {
     const normalizedAddress = address.toLowerCase()
     let serialized3id = localstorage.get(STORAGE_KEY + normalizedAddress)
     if (serialized3id) {
@@ -97,19 +107,24 @@ class ThreeId {
       const entropy = '0x' + utils.sha256(sig.slice(2))
       const mnemonic = HDNode.entropyToMnemonic(entropy)
       const seed = HDNode.mnemonicToSeed(mnemonic)
-      const muport = await MuPort.newIdentity(null, null, {
-        externalMgmtKey: normalizedAddress,
-        mnemonic,
-        ipfsConf: opts.muportIpfs
-      })
       serialized3id = JSON.stringify({
         managementAddress: address,
         seed,
-        spaceSeeds: {},
-        muport: muport.serializeState()
+        spaceSeeds: {}
       })
     }
-    return new ThreeId(serialized3id, ethereum, opts)
+    const _3id = new ThreeId(serialized3id, ethereum, ipfs, opts)
+    await _3id._initMuport(opts.muportIpfs || MUPORT_IPFS)
+    return _3id
+  }
+}
+
+const createMuportDocument = (signingKey, managementKey, asymEncryptionKey) => {
+  return {
+    version: 1,
+    signingKey,
+    managementKey,
+    asymEncryptionKey
   }
 }
 
