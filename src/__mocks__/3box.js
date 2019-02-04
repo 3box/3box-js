3ef376910a6d42a7adc91ee10ebb091b210490f2
Cristóbal Castillo <cristobal.castillo@consensys.net>
2019-02-04 18:42:18 -0300

    add private store to Box mock

--- src/__mocks__/3box.js
+++ src/__mocks__/3box.js
@@ -1,21 +1,24 @@
 class Box {
-
   constructor (muportDID, ethereumProvider, opts = {}) {
     this._publicStore = {}
+    this._privateStore = {}
     this._3id = {
-      getDid : () => muportDID
+      getDid: () => muportDID
     }
     this.public = {
       set: (k, v) => this._publicStore[k] = v,
       get: (k) => this._publicStore[k]
+    },
+    this.private = {
+      set: (k, v) => this._privateStore[k] = v,
+      get: (k) => this._privateStore[k]
     }
   }
 
   static async openBox (address, ethereumProvider, opts = {}) {
-    let did = "did:muport:"+address
-    return new Box(did, ethereumProvider, opts);
+    let did = 'did:muport:' + address
+    return new Box(did, ethereumProvider, opts)
   }
-
 }
 
 module.exports = Box
