fac38902a332fa774bbf622587a59ad8b7097a13
Cristóbal Castillo <cristobal.castillo@consensys.net>
2019-02-04 18:31:57 -0300

    add email to box.verified api

--- src/__tests__/verified.test.js
+++ src/__tests__/verified.test.js
@@ -3,30 +3,28 @@ const Box = require('../3box')
 const didJWT = require('did-jwt')
 const { registerMethod } = require('did-resolver')
 
-const GITHUB_LINK1_URL = "https://gist.githubusercontent.com/user1/12345"
-const GITHUB_LINK1_CONTENT = "some random text did:muport:0x12345 more random text"
-const GITHUB_LINK1_USER = "user1"
-const GITHUB_LINK2_URL = "https://gist.githubusercontent.com/user1/wrongLink"
-const GITHUB_LINK2_CONTENT = "wrong did"
+const GITHUB_LINK1_URL = 'https://gist.githubusercontent.com/user1/12345'
+const GITHUB_LINK1_CONTENT = 'some random text did:muport:0x12345 more random text'
+const GITHUB_LINK1_USER = 'user1'
+const GITHUB_LINK2_URL = 'https://gist.githubusercontent.com/user1/wrongLink'
+const GITHUB_LINK2_CONTENT = 'wrong did'
 
 jest.mock('../3box')
 jest.mock('../utils', () => {
-  const GITHUB_LINK1_URL = "https://gist.githubusercontent.com/user1/12345"
-  const GITHUB_LINK1_CONTENT = "some random text did:muport:0x12345 more random text"
-  const GITHUB_LINK1_USER = "user1"
-  const GITHUB_LINK2_URL = "https://gist.githubusercontent.com/user1/wrongLink"
-  const GITHUB_LINK2_CONTENT = "wrong did"
+  const GITHUB_LINK1_URL = 'https://gist.githubusercontent.com/user1/12345'
+  const GITHUB_LINK1_CONTENT = 'some random text did:muport:0x12345 more random text'
+  const GITHUB_LINK1_USER = 'user1'
+  const GITHUB_LINK2_URL = 'https://gist.githubusercontent.com/user1/wrongLink'
+  const GITHUB_LINK2_CONTENT = 'wrong did'
   return {
     fetchText: jest.fn(async url => {
-        if (url === GITHUB_LINK1_URL) {
-          return GITHUB_LINK1_CONTENT
-        }
-        else if(url === GITHUB_LINK2_URL) {
-          return GITHUB_LINK2_CONTENT
-        }
-        else {
-          throw new Error("ERROR");
-        }
+      if (url === GITHUB_LINK1_URL) {
+        return GITHUB_LINK1_CONTENT
+      } else if (url === GITHUB_LINK2_URL) {
+        return GITHUB_LINK2_CONTENT
+      } else {
+        throw new Error('ERROR')
+      }
     })
   }
 })
@@ -35,17 +33,17 @@ jest.mock('https-did-resolver', () => {
 })
 registerMethod('https', async () => {
   return {
-    "@context": "https://w3id.org/did/v1",
-    "id": "did:https:test.com",
-    "publicKey": [{
-        "id": "did:https:test.com#owner",
-        "type": "Secp256k1VerificationKey2018",
-        "owner": "did:https:test.com",
-        "publicKeyHex": "044f5c08e2150b618264c4794d99a22238bf60f1133a7f563e74fcf55ddb16748159872687a613545c65567d2b7a4d4e3ac03763e1d9a5fcfe512a371faa48a781"
+    '@context': 'https://w3id.org/did/v1',
+    'id': 'did:https:test.com',
+    'publicKey': [{
+      'id': 'did:https:test.com#owner',
+      'type': 'Secp256k1VerificationKey2018',
+      'owner': 'did:https:test.com',
+      'publicKeyHex': '044f5c08e2150b618264c4794d99a22238bf60f1133a7f563e74fcf55ddb16748159872687a613545c65567d2b7a4d4e3ac03763e1d9a5fcfe512a371faa48a781'
     }],
-    "authentication": [{
-        "type": "Secp256k1SignatureAuthentication2018",
-        "publicKey": "did:https:test.com#owner"
+    'authentication': [{
+      'type': 'Secp256k1SignatureAuthentication2018',
+      'publicKey': 'did:https:test.com#owner'
     }]
   }
 })
@@ -56,7 +54,7 @@ describe('Verified', () => {
   let verified
 
   beforeAll(async () => {
-    box = await Box.openBox("0x12345", "web3prov");
+    box = await Box.openBox('0x12345', 'web3prov')
     verified = new Verified(box)
   })
 
@@ -75,7 +73,7 @@ describe('Verified', () => {
     })
 
     it('should throw if gistUrl is empty', async () => {
-      expect(verified.addGithub("")).rejects.toEqual(new Error('The proof of your Github is not available'))
+      expect(verified.addGithub('')).rejects.toEqual(new Error('The proof of your Github is not available'))
     })
   })
 
@@ -110,7 +108,7 @@ describe('Verified', () => {
     it('should add the twitter proof and get the twitter handler to verify if it is verified', async () => {
       await verified.addTwitter(correctClaim)
       let twitter = await verified.twitter()
-      expect(twitter).toEqual({"username": "twitterUser", "proof": "https://twitter.com/twitterUser/12387623", "verifiedBy": "did:https:test.com"})
+      expect(twitter).toEqual({ 'username': 'twitterUser', 'proof': 'https://twitter.com/twitterUser/12387623', 'verifiedBy': 'did:https:test.com' })
     })
 
     it('should throw if twitter claim does not contain the correct did', async () => {
@@ -129,6 +127,59 @@ describe('Verified', () => {
         signer: httpsDidSigner
       })
       expect(verified.addTwitter(incorrectClaim)).rejects.toEqual(new Error('The claim for your twitter is not correct'))
+    })
+  })
+
+  describe('email', () => {
+    let incorrectClaim
+    let correctClaim
+
+    beforeAll(async () => {
+      incorrectClaim = await didJWT.createJWT({
+        sub: 'did:muport:QMasdivuhs',
+        iat: 123456789,
+        claim: {
+          werid_claim: 'some data'
+        }
+      }, {
+        issuer: 'did:https:test.com',
+        signer: httpsDidSigner
+      })
+      correctClaim = await didJWT.createJWT({
+        sub: 'did:muport:0x12345',
+        iat: 123456789,
+        claim: {
+          email_address: 'user@3box.io',
+          code: '123456'
+        }
+      }, {
+        issuer: 'did:https:test.com',
+        signer: httpsDidSigner
+      })
+    })
+
+    it('should add the email proof and get the email address to verify if it is verified', async () => {
+      await verified.addEmail(correctClaim)
+      let email = await verified.email()
+      expect(email).toEqual({ 'email_address': 'user@3box.io', 'proof': '123456', 'verifiedBy': 'did:https:test.com' })
+    })
+
+    it('should throw if email claim does not contain the correct did', async () => {
+      expect(verified.addEmail(incorrectClaim)).rejects.toEqual(new Error('Verification not valid for given user'))
+    })
+
+    it('should throw if twitter claim does not contain email_address and proof', async () => {
+      incorrectClaim = await didJWT.createJWT({
+        sub: 'did:muport:0x12345',
+        iat: 123456789,
+        claim: {
+          werid_claim: 'some data'
+        }
+      }, {
+        issuer: 'did:https:test.com',
+        signer: httpsDidSigner
+      })
+      expect(verified.addEmail(incorrectClaim)).rejects.toEqual(new Error('The claim for your email address is not correct'))
     })
   })
 })
