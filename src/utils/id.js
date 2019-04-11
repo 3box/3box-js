781fd0a4448cfd130a3e393a5549ad79f11e3dba
Laurent Senta <Laurent.Senta@gmail.com>
2019-04-11 11:30:36 +0200

    feat(idUtils): isClaim, verifyClaim

--- src/utils/id.js
+++ src/utils/id.js
@@ -1,5 +1,21 @@
+const didJWT = require('did-jwt')
 const DID_MUPORT_PREFIX = 'did:muport:'
 
 module.exports = {
   isMuportDID: (address) => address.startsWith(DID_MUPORT_PREFIX),
+  isClaim: async (claim, opts = {}) => {
+    try {
+      await didJWT.decodeJWT(claim, opts)
+      return true
+    } catch (e) {
+      return false
+    }
+  },
+  verifyClaim: async (claim, opts = {}) => {
+    try {
+      return await didJWT.verifyJWT(claim, opts)
+    } catch (e) {
+      return null
+    }
+  }
 }
