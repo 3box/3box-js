1b9a5c42103a72d505c7f956acdcd895cdf29ae7
Laurent Senta <Laurent.Senta@gmail.com>
2019-04-16 14:55:18 +0200

    doc: work around idUtils module getting front position

--- src/utils/id.js
+++ src/utils/id.js
@@ -1,7 +1,8 @@
 /**
  * A module to verify & validate claims
  *
- * @module idUtils
+ * @name idUtils
+ * @memberof Box
  */
 const didJWT = require('did-jwt')
 const DID_MUPORT_PREFIX = 'did:muport:'
@@ -10,6 +11,7 @@ module.exports = {
   /**
    * Check whether a string is a muport did or not
    *
+   * @memberOf Box.idUtils
    * @param   {String}     address  A string containing a user profile address
    * @return  {*|boolean}           Whether the address is a muport did or not
    */
@@ -18,6 +20,7 @@ module.exports = {
   /**
    * Check whether a string is a valid claim or not
    *
+   * @memberOf Box.idUtils
    * @param  {String}             claim
    * @param  {Object}             opts            Optional parameters
    * @param  {string}             opts.audience   The DID of the audience of the JWT
@@ -36,6 +39,7 @@ module.exports = {
    * Verify a claim and return its content.
    * See https://github.com/uport-project/did-jwt/ for more details.
    *
+   * @memberOf Box.idUtils
    * @param  {String}             claim
    * @param  {Object}             opts            Optional parameters
    * @param  {string}             opts.audience   The DID of the JWT's audience
