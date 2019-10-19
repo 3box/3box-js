db054b61238dd9c8e662372fd217d5a3ac6eb55a
ghiliweld <ghiliaweld@gmail.com>
2019-10-18 22:43:05 -0400

    fix: initialized _filterFn

--- src/ghost.js
+++ src/ghost.js
@@ -16,6 +16,7 @@ class GhostThread extends EventEmitter {
     this._members = {}
     this._backlog = new Set() // set of past messages
     this._backlogLimit = opts.ghostBacklogLimit || DEFAULT_BACKLOG_LIMIT
+    this._filterFn = () => true
 
     this._room.on('message', async ({ from, data }) => {
       const { payload, issuer } = await this._verifyData(data)
