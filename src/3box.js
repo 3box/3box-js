a49d09a153b714addfd98b4ffbdd0887bee4e5b1
Joel Torstensson <oed3933@gmail.com>
2019-10-21 15:19:29 -0400

    fix(3box): check for window correctly in initIPFSRepo

--- src/3box.js
+++ src/3box.js
@@ -630,7 +630,7 @@ function initIPFSRepo () {
   let ipfsRootPath
 
   // if in browser, create unique root storage, and ipfs id on each instance
-  if (window && window.indexedDB) {
+  if (typeof window !== 'undefined' && window.indexedDB) {
     const sessionID = utils.randInt(10000)
     ipfsRootPath = 'ipfs/root/' + sessionID
     const levelInstance = new LevelStore(ipfsRootPath)
@@ -666,7 +666,7 @@ async function initIPFS (ipfs, iframeStore, ipfsOptions) {
       })
       ipfs.on('ready', () => {
         resolve(ipfs)
-        if (ipfsRepo && window && window.indexedDB) {
+        if (ipfsRepo && typeof window !== 'undefined' && window.indexedDB) {
           // deletes once db is closed again
           window.indexedDB.deleteDatabase(ipfsRepo.rootPath)
         }
