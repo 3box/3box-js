48e22132a5d9bbce12516ad2c6d4e8cf5b37fd0c
Michael Sterle-Contala <2691351+msterle@users.noreply.github.com>
2020-02-12 12:51:41 +0100

    clear polling interval on shutdown

--- src/3id/index.js
+++ src/3id/index.js
@@ -35,7 +35,7 @@ class ThreeId {
           this.events.emit(event, data)
         })
       }
-      setInterval(() => {
+      this._pollInterval = setInterval(() => {
         poll('3id_newAuthMethodPoll', 'new-auth-method')
         poll('3id_newLinkPoll', 'new-link-proof')
       }, POLL_INTERVAL)
@@ -272,6 +272,9 @@ class ThreeId {
 
   logout () {
     localstorage.remove(STORAGE_KEY + this.managementAddress)
+    if (this._pollInterval) {
+      clearInterval(this._pollInterval)
+    }
   }
 
   static isLoggedIn (address) {
