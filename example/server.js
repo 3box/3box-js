a24ed2956a9ce721889179691da1bc64365c4acc
Dr Washington Sanchez <washington.sanchez@gmail.com>
2020-07-14 13:11:50 +1000

    fix: Use port number indicated in the documentation
    https://docs.3box.io/try/example-app

--- example/server.js
+++ example/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-const port = 30000
+const port = 3000
 
 const app = express()
 app.use(serveStatic(path.join(__dirname), {'index': ['index.html']}))
